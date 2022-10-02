5672968975234295e6ce87ffeea8d21531dcb851
Daiskz <gabriel.matsubara@gmail.com>
2022-10-02 13:03:34 -0300

    criação do projeto

---
+++ src/App.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class App
+{
+    public static void main(String[] args) throws Exception 
+    {
+        int notas[] = new int[4];
+        int i;
+        float total=0, média;
+        try (Scanner sc = new Scanner(System.in)) {
+            for(i=0; i<4; i++) {
+                System.out.print("Digite a nota " +(i+1)+":");
+                notas[i] = sc.nextInt();
+                total = total + notas[i];
+   }
+   sc.close();
+        }
+        média = total/4;
+    System.out.print("A média do estudante é: ");
+    if(média>=80)
+    {
+        System.out.print("A");
+    }   
+    else if(média>=60 && média<80)
+    {
+        System.out.print("B");
+    }
+    else if(média>=40 && média<60)
+    {
+        System.out.print("C");
+    }
+    else   
+    {
+        System.out.print("D");
+    }
+  }
+}
