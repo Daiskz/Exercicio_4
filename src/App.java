35c627bed0dd6dd57478034d9d6fc7785ef7121a
Daiskz <gabriel.matsubara@gmail.com>
2022-10-27 03:59:22 -0300

    Mudança no código

--- src/App.java
+++ src/App.java
@@ -4,24 +4,24 @@ public class App
 {
     public static void main(String[] args) throws Exception 
     {
-        int notas[] = new int[4];
+        int notas[] = new int [4];
         int i;
-        float total=0, média;
-        try (Scanner sc = new Scanner(System.in)) {
-            for(i=0; i<4; i++) {
-                System.out.print("Digite a nota " +(i+1)+":");
-                notas[i] = sc.nextInt();
-                total = total + notas[i];
+        float total = 0, média;
+        try (Scanner entrada = new Scanner(System.in)) {
+            for(i = 0; i < 4; i++) {
+                System.out.print("Digite a nota " + (i+1) +":");
+                notas [i] = entrada.nextInt();
+                total = total + notas [i];
    }
-   sc.close();
+   entrada.close();
         }
         média = total/4;
     System.out.print("A média do estudante é: ");
-    if(média>=80)
+    if (média >= 80)
     {
         System.out.print("A");
     }   
-    else if(média>=60 && média<80)
+    else if(média >= 60 && média < 80)
     {
         System.out.print("B");
     }
